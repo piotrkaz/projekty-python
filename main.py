170e63c4820c9269ca0be36314cd53ecd1330a37
Piotrkaz <piotrkaz@gmail.com>
2023-09-27 22:02:51 +0200

    save

--- main.py
+++ main.py
@@ -14,14 +14,18 @@ print('Zrzucono dane do pliku.')
 
 temp2 = json.dumps(data[0]['rates'])
 
-with open('mid_rates.json', 'w') as file:
+with open('mid_rates_dirty.json', 'w') as file:
     file.write(temp2)
 
 print('Zapisano srednie kursy walut.')
 print(f'Oto kursy walut z dnia: {date}')
 
-with open('mid_rates.json') as f:
+with open('mid_rates_dirty.json') as f:
     rates = json.load(f)
 
 for rate in rates:
-    print(rate['code'], ' ', rate['mid'])
+    del rate['currency']
+    print(rate['code'], '   ', rate['mid'])
+
+with open('mid_rates.json', 'w') as file:
+    json.dump(rates, file)
