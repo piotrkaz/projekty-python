59e9483c2e82c124620ad63217abf45946b7d2b9
Piotrkaz <piotrkaz@gmail.com>
2023-09-27 19:21:18 +0200

    małe zmiany

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ date = '2023-09-27'
 url = f'http://api.nbp.pl/api/exchangerates/tables/{table}/{date}/?format=json'
 print(url)
 response = requests.get(url)
-response_json = response.json()
-print(response_json)
+data = response.json()
+print(data)
 
-temp_file = json.dumps(response_json)
-with open('zrzut.json', 'w') as file:
+temp_file = json.dumps(data)
+with open('dump_file.json', 'w') as file:
     file.write(temp_file)
