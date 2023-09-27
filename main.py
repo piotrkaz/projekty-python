c96d9b2e54a7abb398c8c1710a31c9b0bc7a1002
Piotrkaz <piotrkaz@gmail.com>
2023-09-27 19:23:43 +0200

    save

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import json
 table = 'a'
 date = '2023-09-27'
 url = f'http://api.nbp.pl/api/exchangerates/tables/{table}/{date}/?format=json'
-print(url)
 response = requests.get(url)
 data = response.json()
-print(data)
 
 temp_file = json.dumps(data)
 with open('dump_file.json', 'w') as file:
     file.write(temp_file)
+print('Zrzucono dane do pliku')
