41220b3f9fd046efcadd8861c056bfaea0a48caf
Piotrkaz <piotrkaz@gmail.com>
2023-09-27 19:17:44 +0200

    nastepny save

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-imie = 'Piotr'
-print(f'Hello {imie}!')
-wiek = input('Wprowadź swój wiek: ')
-print(f'Twój wiek to: {wiek} lat.')
+import requests
+import json
+
+table = 'a'
+date = '2023-09-27'
+url = f'http://api.nbp.pl/api/exchangerates/tables/{table}/{date}/?format=json'
+print(url)
+response = requests.get(url)
+response_json = response.json()
+print(response_json)
+
+temp_file = json.dumps(response_json)
+with open('zrzut.json', 'w') as file:
+    file.write(temp_file)
